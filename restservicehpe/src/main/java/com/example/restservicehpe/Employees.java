91fbb0aabff2b4e8d05d50f08594fe1e83392bcf
Jajhoey <john.hoey15@my.stjohns.edu>
2024-04-18 12:11:05 -0400

    Created classes for employee objects and employee list

--- restservicehpe/src/main/java/com/example/restservicehpe/Employees.java
+++ restservicehpe/src/main/java/com/example/restservicehpe/Employees.java
@@ -1,7 +1,23 @@
 package com.example.restservicehpe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Employees {
-    private List<Employees> employees;
+
+    private List<Employee> employees;
+
+    //Getter and setter
+    //Including a null case
+    public List<Employee> getEmployees(){
+        if(this.employees == null){
+            this.employees = new ArrayList<Employee>();
+        }
+
+        return this.employees;
+    }
+
+    public void setEmployees(List<Employee> employees){
+        this.employees = employees;
+    }
 }
