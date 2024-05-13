5975e1559d0bf350fa2409db663e2bb52132c8fa
Jajhoey <john.hoey15@my.stjohns.edu>
2024-05-13 12:13:40 -0400

    Excluding data source auto config. JPA was trying to use a db connection

--- restservicehpe/src/main/java/com/example/restservicehpe/RestservicehpeApplication.java
+++ restservicehpe/src/main/java/com/example/restservicehpe/RestservicehpeApplication.java
@@ -2,8 +2,9 @@ package com.example.restservicehpe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class RestservicehpeApplication {
 
 	public static void main(String[] args) {
--- restservicehpe/src/test/java/com/example/restservicehpe/RestservicehpeApplicationTests.java
+++ restservicehpe/src/test/java/com/example/restservicehpe/RestservicehpeApplicationTests.java
@@ -3,10 +3,7 @@ package com.example.restservicehpe;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
-/* 
-What to test
-	
-**/
+
 @SpringBootTest
 class RestservicehpeApplicationTests {
 
